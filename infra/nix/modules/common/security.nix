e1e386bd659f8d61615d1e661a593a16b27eca84
서창덕 <44630705+Garodden@users.noreply.github.com>
2025-08-19 22:00:42 +0900

    del: reference config files delet fix: security to avoid conflict with aws ec2 image

--- infra/nix/modules/common/security.nix
+++ infra/nix/modules/common/security.nix
@@ -6,7 +6,6 @@
   services.openssh = {
     enable = true;
     settings = {
-      PermitRootLogin = "no";
       PasswordAuthentication = false;
       KbdInteractiveAuthentication = false;
       X11Forwarding = false;
