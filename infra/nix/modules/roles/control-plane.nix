921077d3c49a9a9ef70140df27f8ef0f4aa5a607
Seo Chang Deok <seocd777@gmail.com>
2025-09-13 14:15:12 +0900

    fix: Disable default CNI plugins in cri-o

--- infra/nix/modules/roles/control-plane.nix
+++ infra/nix/modules/roles/control-plane.nix
@@ -1,11 +1,10 @@
-{ config, pkgs, ... }:
+{ config, pkgs, lib, ... }:
 
 {
-  virtualisation.cri-o = {
-    enable = true;
-    settings.crio.network.network_dir = "";
-    settings.crio.network.plugin_dirs = [];
-  };
+  virtualisation.cri-o.enable = true;
+  
+  environment.etc."cni/net.d/10-crio-bridge.conflist".enable = lib.mkForce false;
+  environment.etc."cni/net.d/99-loopback.conflist".enable = lib.mkForce false;
 
   environment.systemPackages = with pkgs; [
     kubernetes
