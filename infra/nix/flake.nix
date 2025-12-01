7adc979a66755ecae7a7641f8962f5b9a8a138b7
duckoriduck <seocd777@gmail.com>
2025-12-01 13:12:41 +0900

    update: nixos 25.05 -> 25.11

--- infra/nix/flake.nix
+++ infra/nix/flake.nix
@@ -2,7 +2,7 @@
   description = "DucksNest Homelab NixOS Configuration";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
     nixos-generators = {
       url = "github:nix-community/nixos-generators";
